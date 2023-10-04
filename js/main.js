66793283c608f0b18794a9c5787c8c7d76c66988
HIITMEMARIO <alswnsgur@nate.com>
2023-10-04 21:01:07 +0900

    index.html 애니메이션 추가

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+// index.html 이미지,텍스트 순차적으로 보이게 하는 애니메이션 효과
+
+const fadeEls = document.querySelectorAll('.gsap')
+
+fadeEls.forEach(function (fadeEl, index){
+  gsap.to(fadeEl, 1, {  
+    delay:(index + 1) * .5, 
+    opacity: 1
+  })
+});
+
+
+
