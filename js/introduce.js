4087c9f8cbaabf0d7c94887008717dbda5610b96
김제틴 <kimjt@MH-MacBookAir.local>
2023-10-05 15:02:31 +0900

    다크모드 & top 버튼

--- js/introduce.js
+++ js/introduce.js
@@ -1,5 +1,24 @@
-const darkMode = document.querySelector('#dark_mode');
+const darkModeBtn = document.querySelector('#dark_mode_checkbox');
 
-darkMode.addEventListener('click', (e) => {
-  document.documentElement.setAttribute('color-theme', 'dark');
+let localStorageValue = localStorage.getItem('color-theme');
+
+window.onload = function () {
+  if (localStorageValue) {
+    document.documentElement.setAttribute(
+      'color-theme',
+      `${localStorageValue}`
+    );
+  } else {
+    localStorage.setItem('color-theme', 'light');
+  }
+};
+
+darkModeBtn.addEventListener('click', (e) => {
+  if (e.target.checked) {
+    localStorage.setItem('color-theme', 'dark');
+    document.documentElement.setAttribute('color-theme', 'dark');
+  } else {
+    localStorage.setItem('color-theme', 'light');
+    document.documentElement.setAttribute('color-theme', 'light');
+  }
 });
