e978043d639fe42a24abde10b71b0a8bb9ae450e
mi-hee-k <90510192+mi-hee-k@users.noreply.github.com>
2023-10-05 18:02:00 +0900

    부트스트랩 삭제 & css 수정

--- js/introduce.js
+++ js/introduce.js
@@ -3,13 +3,12 @@ const darkModeBtn = document.querySelector('#dark_mode_checkbox');
 let localStorageValue = localStorage.getItem('color-theme');
 
 window.onload = function () {
+  document.documentElement.setAttribute('color-theme', 'light');
   if (localStorageValue) {
     document.documentElement.setAttribute(
       'color-theme',
       `${localStorageValue}`
     );
-  } else {
-    localStorage.setItem('color-theme', 'light');
   }
 };
 
