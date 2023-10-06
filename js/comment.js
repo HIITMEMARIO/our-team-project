9742dd55ba3324c372a152689d2652982ae51ce3
hyewon-han <hw1635@hanmail.net>
2023-10-06 15:42:07 +0900

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -1,40 +1,50 @@
-const username = document.querySelector(".comments__username");
-const comment = document.querySelector(".comments__textarea");
-const submitBtn = document.getElementById("comments__submit_btn");
-
-function handleclick() {
-  alert("hi");
-}
-
-submitBtn.addEventListener("click", handleclick);
-
 // Firebase SDK 라이브러리 가져오기
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import {
+  getFirestore,
   collection,
   addDoc,
-} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import {
   getDocs,
+  getDoc,
   deleteDoc,
-} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+  Timestamp,
+  doc,
+  orderBy,
+  query,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 
 // Firebase 구성 정보 설정
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBFMv6kAjohiJW_DBDxaEDyj0pZ2nvA_e4",
-  authDomain: "sajochamchi-9d4df.firebaseapp.com",
-  projectId: "sajochamchi-9d4df",
-  storageBucket: "sajochamchi-9d4df.appspot.com",
-  messagingSenderId: "216151407535",
-  appId: "1:216151407535:web:2152ad1ea2af792c6a4d5a",
-  measurementId: "G-KC16MXH9ZV",
+  apiKey: "AIzaSyCA_W0Kd8-Quj8vGsbgQ5zcl7IIfm4YGjA",
+  authDomain: "sparta-816b8.firebaseapp.com",
+  projectId: "sparta-816b8",
+  storageBucket: "sparta-816b8.appspot.com",
+  messagingSenderId: "838877346235",
+  appId: "1:838877346235:web:86b6f2a1013b0a66612c9e",
+  measurementId: "G-P881GFJ7Z6",
 };
 
 // Firebase 인스턴스 초기화
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const ref = collection(db, "nbcamp1stProject");
+/*
+const submitBtn = document.getElementById("submit_btn");
+const username = document.getElementById("username");
+const comment = document.getElementById("textarea");
 
-$("comments__submit_btn").click(async function () {});
+submitBtn.addEventListener("click", async function (e) {
+  console.log(e);
+});
+*/
+
+$("submit_btn").click(async function () {
+  const username = $("#username").val();
+  const textarea = $("#textarea").val();
+
+  let doc = { username: username, textarea: textarea };
+  await addDoc(collection(db, "comments"), doc);
+  alert("저장 완료!");
+  window.location.reload();
+});
